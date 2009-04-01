4f0ff7b20c8b2033ff6895bbe148b4cc16459b33
bartbes <bart.bes+projects@gmail.com>
2009-04-01 09:07:35 +0200

    Added directory structure and a sample object, RFC

--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
 function load()
 	love.graphics.setFont(love.default_font)
+	local mods = love.filesystem.enumerate("mods")
+	for i, v in ipairs(mods) do
+		love.filesystem.require(v)
+	end
 end
 
 function draw()
