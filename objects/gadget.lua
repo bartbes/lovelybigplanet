1ea108f6f614cced22ea7cb012119343ca9dbbd1
gvx <gyvox.public@gmail.com>
2009-07-11 19:53:15 +0200

    Added gadget Object, which is something to push around for the player once we have controls

---
+++ objects/gadget.lua
@@ -0,0 +1,11 @@
+OBJECT.Name = "Gadget"
+OBJECT.Creator = "Robin Wellner"
+OBJECT.Version = 0.0
+OBJECT.Resources = { texture = "player" }
+OBJECT.TextureScale = { x = .4 }
+
+OBJECT.Weight = 5
+OBJECT.Polygon = { {-.2, -.2, -.2, .2, .2, .2, .2, -.2} }
+
+function OBJECT:collision(a)
+end
