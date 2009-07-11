bc3ada8c59663e12b0224ea22d9f88ea8143672f
bartbes <bart.bes+projects@gmail.com>
2009-07-11 14:52:22 +0200

    Upscaling, default format is now 150 px/m

--- API.lua
+++ API.lua
@@ -1,5 +1,5 @@
 LBP = {}
 
 function LBP.draw(object)
-	love.graphics.draw(object.Resources.texture, object._body:getX(), object._body:getY(), object._body:getAngle(), object.TextureScale.x/10, (object.TextureScale.y or object.TextureScale.x)/10)
+	love.graphics.draw(object.Resources.texture, object._body:getX(), object._body:getY(), object._body:getAngle(), object.TextureScale.x/150, (object.TextureScale.y or object.TextureScale.x)/150)
 end
