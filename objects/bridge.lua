142f69eb438626bfda7fb54043a156f3180c4866
gvx <gyvox.public@gmail.com>
2009-07-12 16:25:19 +0200

    Some changes to test the level

--- objects/bridge.lua
+++ objects/bridge.lua
@@ -1,11 +1,11 @@
-OBJECT.Name = "Gadget"
+OBJECT.Name = "Bridge"
 OBJECT.Creator = "Robin Wellner"
 OBJECT.Version = 0.0
 OBJECT.Resources = { texture = "block" }
-OBJECT.TextureScale = { x = .8 }
+OBJECT.TextureScale = { x = 2.8 , y = .8}
 
 OBJECT.Static = false
-OBJECT.Polygon = { {-.4, -.4, -.4, .4, .4, .4, .4, -.4} }
+OBJECT.Polygon = { {-1.4, -.4, -1.4, .4, 1.4, .4, 1.4, -.4} }
 
 function OBJECT:collision(a)
 end
