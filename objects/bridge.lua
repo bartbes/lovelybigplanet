da2a81cd643def9272a10ed21044f4bd52f521e2
Torquil MacLeod <torquil@torquil-laptop.(none)>
2009-07-13 20:16:37 +0100

    Fixed my fix of a fix (^_^)

--- objects/bridge.lua
+++ objects/bridge.lua
@@ -2,7 +2,7 @@ OBJECT.Name = "Bridge"
 OBJECT.Creator = "Robin Wellner"
 OBJECT.Version = 0.0
 OBJECT.Resources = { texture = "longblock" }
-OBJECT.TextureScale = { x = 3 , y = 1}
+OBJECT.TextureScale = { x = 1 , y = 1}
 
 OBJECT.Static = false
 OBJECT.Polygon = { {-1.4, -.4, -1.4, .4, 1.4, .4, 1.4, -.4} }
