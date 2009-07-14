9f51d62d751650e3e680210d6a18ed07b0ca0f7b
bartbes <bart.bes+projects@gmail.com>
2009-07-14 10:37:09 +0200

    Optimized a bit (only one call to love.graphics.getWidth() in hud.draw())

--- hud.lua
+++ hud.lua
@@ -3,11 +3,12 @@ hud = {}
 function hud.draw()
 	setCamera(cameras.hud)
 	local col = love.graphics.getColor()
+	local width = love.graphics.getWidth()
 	love.graphics.setColor(175, 175, 50)
 	if hud.score or dbg then
-		love.graphics.rectangle(love.draw_fill, love.graphics.getWidth()/2-200, 0, 400, 50)
-		love.graphics.triangle(love.draw_fill, love.graphics.getWidth()/2-200, 0, love.graphics.getWidth()/2-200, 50, love.graphics.getWidth()/2-250, 0)
-		love.graphics.triangle(love.draw_fill, love.graphics.getWidth()/2+200, 0, love.graphics.getWidth()/2+200, 50, love.graphics.getWidth()/2+250, 0)
+		love.graphics.rectangle(love.draw_fill, width/2-200, 0, 400, 50)
+		love.graphics.triangle(love.draw_fill, width/2-200, 0, width/2-200, 50, width/2-250, 0)
+		love.graphics.triangle(love.draw_fill, width/2+200, 0, width/2+200, 50, width/2+250, 0)
 	end
 	if hud.lvl1 or dbg then
 		love.graphics.rectangle(love.draw_fill, 0, 0, 100, 30)
@@ -15,15 +16,15 @@ function hud.draw()
 		love.graphics.triangle(love.draw_fill, 80, 30, 100, 30, 80, 50)
 	end
 	if hud.lvl2 or dbg then
-		love.graphics.rectangle(love.draw_fill, love.graphics.getWidth()-100, 0, 100, 30)
-		love.graphics.rectangle(love.draw_fill, love.graphics.getWidth()-80, 30, 80, 20)
-		love.graphics.triangle(love.draw_fill, love.graphics.getWidth()-80, 30, love.graphics.getWidth()-80, 50, love.graphics.getWidth()-100, 30)
+		love.graphics.rectangle(love.draw_fill, width-100, 0, 100, 30)
+		love.graphics.rectangle(love.draw_fill, width-80, 30, 80, 20)
+		love.graphics.triangle(love.draw_fill, width-80, 30, width-80, 50, width-100, 30)
 	end
 	love.graphics.setColor(0, 0, 0)
 	if hud.score or dbg then
-		love.graphics.line(love.graphics.getWidth()/2-250, 0, love.graphics.getWidth()/2-200, 50)
-		love.graphics.line(love.graphics.getWidth()/2-200, 50, love.graphics.getWidth()/2+200, 50)
-		love.graphics.line(love.graphics.getWidth()/2+200, 50, love.graphics.getWidth()/2+250, 0)
+		love.graphics.line(width/2-250, 0, width/2-200, 50)
+		love.graphics.line(width/2-200, 50, width/2+200, 50)
+		love.graphics.line(width/2+200, 50, width/2+250, 0)
 	end
 	if hud.lvl1 or dbg then
 		love.graphics.line(0, 50, 80, 50)
@@ -31,16 +32,16 @@ function hud.draw()
 		love.graphics.line(80, 50, 100, 30)
 	end
 	if hud.lvl2 or dbg then
-		love.graphics.line(love.graphics.getWidth(), 50, love.graphics.getWidth()-80, 50)
-		love.graphics.line(love.graphics.getWidth()-100, 0, love.graphics.getWidth()-100, 30)
-		love.graphics.line(love.graphics.getWidth()-100, 30, love.graphics.getWidth()-80, 50)
+		love.graphics.line(width, 50, width-80, 50)
+		love.graphics.line(width-100, 0, width-100, 30)
+		love.graphics.line(width-100, 30, width-80, 50)
 	end
 	if hud.score then
-		love.graphics.drawf(tostring(game.score), love.graphics.getWidth()/2, 30, 20, love.align_center)
+		love.graphics.drawf(tostring(game.score), width/2, 30, 20, love.align_center)
 	end
 	if dbg then
 		love.graphics.setColor(255, 0, 0)
-		local x = love.graphics.getWidth()-120
+		local x = width-120
 		local basey = love.graphics.getHeight()/2
 		local line = love.graphics.getFont():getHeight() + 2
 		love.graphics.draw("FPS: " .. love.timer.getFPS(), x, basey)
