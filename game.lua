2bea6ab4aadd8872e7cc4ba71867a08b80b5d6fe
bartbes <bart.bes+projects@gmail.com>
2009-08-13 21:38:46 +0200

    Fixed bug where it never finishes when there is no finished() callback

--- game.lua
+++ game.lua
@@ -59,7 +59,7 @@ function game.update(dt)
 	local x, y = game.map.Objects.player._body:getPosition()
 	if not game.finished and math.floor(x+0.5) == game.map.Finish.x and math.floor(y+0.5) == game.map.Finish.y and game.map.Objects.player._shapes[1]:getCategory() == game.map.Finish.position and math.abs(select(2, game.map.Objects.player._body:getVelocity())) < 0.01 then
 		--is there a map callback, if so, call it
-		if game.map.finished and game.map.finished() then
+		if (game.map.finished and game.map.finished()) or (not game.map.finished) then
 			game.finished = true
 			game.score = game.score + 10000
 		end
