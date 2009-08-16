cc04b988418fed660d673f0351a07584a409218e
bartbes <bart.bes+projects@gmail.com>
2009-08-16 17:23:21 +0200

    Integrated the texture, polygons (bounding boxes) untested

--- objects/helpsign.lua
+++ objects/helpsign.lua
@@ -1,11 +1,11 @@
 OBJECT.Name = "Help Sign"
 OBJECT.Creator = "Bart Bes"
 OBJECT.Version = 0.1
-OBJECT.Resources = { texture = "" } --request pending
+OBJECT.Resources = { texture = "qubodup/sign_yellow-80x100" } --took the yellow one, not definitive
 OBJECT.TextureScale = { x = 1 }
 
 OBJECT.Static = true
-OBJECT.Polygon = {  } --after texture is created
+OBJECT.Polygon = { { -1/15, -1/3, -1/15, 1/3, 1/15, -1/3, 1/15, 1/3 }, { -1/3.75, 1/3.75, 1/7.5, 1/3.75, 1/3.75, 1/3.75, 1/7.5, 0,-1/3.75, 0 } }
 
 hashelped = false
 helptext = "<Help message empty>"
