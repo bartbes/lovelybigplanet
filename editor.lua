b3f5fa985a0a502e9ae4809ef51d8f6492d80f5b
gvx <gyvox.public@gmail.com>
2009-08-16 16:39:45 +0200

    Added some more editor skeleton

--- editor.lua
+++ editor.lua
@@ -3,3 +3,15 @@ editor = {}
 function editor.draw()
 	setCamera(cameras.editor)
 end
+
+function editor.mousepressed(x, y, button)
+	-- trigger some gui action
+end
+
+function editor.enable()
+	
+end
+
+function editor.disable()
+	
+end
