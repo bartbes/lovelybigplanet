703aa698e83dc4457bc5e91f514bbb41b335967f
bartbes <bart.bes+projects@gmail.com>
2009-08-17 15:40:55 +0200

    Added finish object, removed requests for finish

---
+++ objects/finish.lua
@@ -0,0 +1,8 @@
+OBJECT.Name = "Finish"
+OBJECT.Creator = "Bart Bes"
+OBJECT.Version = 0.1
+OBJECT.Resources = { texture = "qubodup/darts_target" }
+OBJECT.TextureScale = { x = 1 }
+
+OBJECT.Static = true
+OBJECT.Circle = { { 1/3 } }
