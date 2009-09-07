d1c5335a13bb6afa862a9ef770116a6e3478f9a8
bartbes <bart.bes+projects@gmail.com>
2009-09-07 20:51:48 +0200

    Should autostart a resource called ambience

--- game.lua
+++ game.lua
@@ -25,6 +25,7 @@ function startgame(map, noplay)
 	game.score = 0
 	getCamera():setOrigin(game.map.Objects.player._body:getX()-love.graphics.getWidth()/2, game.map.Objects.player._body:getY()-love.graphics.getHeight()/2)
 	if game.map.Mission and not noplay then LBP.messageBox(game.map.Mission) end
+	if game.map.Resources.ambience and not noplay then LBP.play(game.map.Resources.ambience) end
 	LBP.showScore(game.map.ShowScore)
 	if game.map.init then game.map.init() end
 end
