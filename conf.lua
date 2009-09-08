4066cc7eb5a5b44beff26d1ed4f12ffd7f89bbae
gvx <gyvox.public@gmail.com>
2009-09-08 18:06:14 +0200

    Changed config for LOVE 0.6.0

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+	t.title = "LovelyBigPlanet"
+	t.author = "Bart Bes & others (see README)"
+	t.screen.width = 1280
+	t.screen.height = 720
+	--t.screen.fullscreen = false
+	--t.screen.vsync = ?
+	--t.screen.fsaa = ?
+end
