df829aa77990ab41cd3ff19c85e25519cbc73f25
bartbes <bart.bes+projects@gmail.com>
2009-09-21 08:08:39 +0200

    Now creates tiny window before switching to final resolution
    Fixes a reported segfault, temporary solution

--- conf.lua
+++ conf.lua
@@ -1,9 +1,11 @@
 function love.conf(t)
 	t.title = "LovelyBigPlanet"
 	t.author = "Bart Bes & others (see README)"
-	t.screen = false
-	--t.screen.width = 1280
-	--t.screen.height = 720
+	--apparently the following line causes segfaults
+	--t.screen = false
+	--so, we'll create a tiny temporary window
+	t.screen.width = 1
+	t.screen.height = 1
 	--t.screen.fullscreen = false
 	--t.screen.vsync = ?
 	--t.screen.fsaa = ?
