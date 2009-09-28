d989c5172f66d2ef33428f835eb2853996008fd1
gvx <gyvox.public@gmail.com>
2009-09-28 20:21:05 +0200

    Added floating object

--- objects/floatinggadget.lua
+++ objects/floatinggadget.lua
@@ -1,4 +1,4 @@
-OBJECT.Name = "Gadget"
+OBJECT.Name = "Floating Gadget"
 OBJECT.Creator = "Robin Wellner"
 OBJECT.Version = 0.0
 OBJECT.Resources = { texture = "snakeface/block" }
@@ -8,4 +8,8 @@ OBJECT.Static = false
 OBJECT.Polygon = { {-.4, -.4, -.4, .4, .4, .4, .4, -.4} }
 
 function OBJECT:collision(a)
+end
+function OBJECT:Init()
+	OBJECT._body:setLinearDamping(100000)
+	OBJECT._body:setAngularDamping(100000)
 end
