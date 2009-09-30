26c0a79a88d1ae09b518bf833e336ec46feccc85
bartbes <bart.bes+projects@gmail.com>
2009-09-30 17:23:02 +0200

    Added autofinish function to finish object
    It checks if you are near the finish, if so it will call LBP.finish

--- objects/finish.lua
+++ objects/finish.lua
@@ -6,3 +6,11 @@ OBJECT.TextureScale = { x = 1 }
 
 OBJECT.Static = true
 OBJECT.Circle = { { 1/3 } }
+
+function OBJECT.autofinish(player)
+	local px, py = LBP.getX(player), LBP.getY(py)
+	local fx, fy = LBP.getX(self), LBP.getY(self)
+	if math.abs(px-fx) < 0.5 and math.abs(py-fy) < 0.5 then
+		LBP.finish()
+	end
+end
