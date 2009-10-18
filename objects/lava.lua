af66b70635fdb5ec8eb7001c0ce67bbc42ca3f38
gvx <gyvox.public@gmail.com>
2009-10-18 20:19:42 +0200

    Fixed lava polygon

--- objects/lava.lua
+++ objects/lava.lua
@@ -3,7 +3,7 @@ OBJECT.Creator = "Bart van Strien"
 OBJECT.Version = 1.0
 OBJECT.Resources = { texture = "bartbes/lava" }
 OBJECT.TextureScale = { x = 1 }
-OBJECT.Polygon = { { -1.5, -1.5, -1.5, -0.5, -0.5, -0.5, -0.5, -1.5 } }
+OBJECT.Polygon = { { -1.5, -.5, -1.5, .5, -.5, .5, -.5, -.5 } }
 OBJECT.Static = true
 
 function OBJECT:collision(a)
